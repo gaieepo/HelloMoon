8f2041714ff44a58a60efae056fee46b33d89967
gaieepo <gaieepo@gmail.com>
2016-06-10 21:47:03 +0800

    basic audio player play and stop

--- src/com/example/HelloMoon/HelloMoonActivity.java
+++ src/com/example/HelloMoon/HelloMoonActivity.java
@@ -2,14 +2,15 @@ package com.example.HelloMoon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class HelloMoonActivity extends Activity {
+public class HelloMoonActivity extends FragmentActivity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_hello_moon);
     }
 }
