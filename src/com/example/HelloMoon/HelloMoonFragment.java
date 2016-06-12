94f6ea01c66c1176eb24f33cf4f44bffd6e38fd8
gaieepo <gaieepo@gmail.com>
2016-06-12 19:35:43 +0800

    retain fragment

--- src/com/example/HelloMoon/HelloMoonFragment.java
+++ src/com/example/HelloMoon/HelloMoonFragment.java
@@ -18,6 +18,12 @@ public class HelloMoonFragment extends Fragment {
     private SurfaceView mSurfaceView;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
